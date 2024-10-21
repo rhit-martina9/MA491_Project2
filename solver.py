57941015e75c85b6d69061ba8acc6c60caf1caef
Tony Martin <martina9@rose-hulman.edu>
2024-10-20 21:45:07 -0400

    hidden singles wip

--- solver.py
+++ solver.py
@@ -16,46 +16,71 @@ def get_row(num, grid):
     return grid[num-1]
 def get_col(num, grid):
     return [row[num-1] for row in grid]
-def get_box_coord(cell):
+def get_box_num(cell):
     row, col = cell
     return int(row / 3)*3 + int(col/3)
-def get_box(num, grid):
+def get_box_cells(box_num):
     out = []
-    base = (3*int(num/3),3*int(num%3))
+    base = (box_num - box_num%3, 3*(box_num%3))
     for row in range(3):
         for col in range(3):
-            out.append(grid[base[0]+row][base[1]+col])
+            out.append((base[0]+row,base[1]+col))
     return out
+def get_box_values(box_num, grid):
+    return [grid[i][j] for i,j in get_box_cells(box_num)]
 
-def get_candidates(cell, grid):
-    candidates = [1,2,3,4,5,6,7,8,9]
+def check_valid(cell, num, grid):
     row, col = cell
     if grid[row][col] > 0:
-        return []
+        return False
     for i in range(9):
-        if grid[i][col] in candidates:
-            candidates.remove(grid[i][col])
-    for j in range(9):
-        if grid[row][j] in candidates:
-            candidates.remove(grid[row][j])
-    box = get_box(get_box_coord(cell), grid)
-    for x in box:
-        if x in candidates:
-            candidates.remove(x)
-    return candidates
-
-def simple_steps(grid):
-    new_grid = [['123456789' for j in range(9)] for i in range(9)]
+        if grid[i][col] == num:
+            return False
+    for i in range(9):
+        if grid[row][i] == num:
+            return False
+    for x in get_box_values(get_box_num(cell), grid):
+        if x == num:
+            return False
+    return True
+def get_candidates(cell, grid):
+    cands = [i+1 for i in range(9)]
+    return list(filter(lambda v: check_valid(cell,v,grid), cands))
+    
+def find_number_locations(num, grid):
+    locations = []
     for row in range(9):
         for col in range(9):
-            print('hi')
-    return new_grid
+            if grid[row][col] == num:
+                locations.append((row,col))
+                continue
+    return locations
+
+def find_hidden_singles(grid):
+    hidden_singles = {i+1:[] for i in range(9)}
+    for num in range(9):
+        for row in range(9):
+            possible = list(filter(lambda cell: check_valid(cell, num, grid), [(row,col) for col in range(9)]))
+            if len(possible) == 1:
+                hidden_singles[num].append(possible[0])
+        for col in range(9):
+            possible = list(filter(lambda cell: check_valid(cell, num, grid), [(row,col) for row in range(9)]))
+            if len(possible) == 1:
+                hidden_singles[num].append(possible[0])
+        for box_num in range(9):
+            possible = list(filter(lambda cell: check_valid(cell, num, grid), get_box_cells(box_num)))
+            if len(possible) == 1:
+                hidden_singles[num].append(possible[0])
+    return hidden_singles
 
 grid = load_grid()
 for line in grid:
     print(line)
 
 print()
-for row in range(9):
-    for col in range(9):
-        print(get_candidates((row,col), grid))
+print(check_valid((0,6),1,grid))
+print()
+# for row in range(9):
+#     for col in range(9):
+#         print(get_candidates((row,col),grid))
+print(find_hidden_singles(grid))
